a0855bff152577a366a4fa778683766e47b0aa9c
Frodoq <the.vaca@mail.ru>
2025-06-30 17:59:11 +0000

    dz_netology_02

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -1,6 +1,23 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = "~>1.8.4"
+}
+
+provider "yandex" {
+  service_account_key_file = "key.json"
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.default_zone
+}
+
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
+
 resource "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
@@ -8,17 +25,17 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
-
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_web_image_family
 }
+
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standart-v4"
+  name        = var.vm_web_name
+  platform_id = var.vm_web_platform_id
   resources {
-    cores         = 1
-    memory        = 1
-    core_fraction = 5
+    cores         = var.vm_web_cores
+    memory        = var.vm_web_memory
+    core_fraction = var.vm_web_core_fraction
   }
   boot_disk {
     initialize_params {
@@ -26,16 +43,19 @@ resource "yandex_compute_instance" "platform" {
     }
   }
   scheduling_policy {
-    preemptible = true
+    preemptible = var.vm_web_preemptible
   }
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
+    nat       = var.vm_web_nat
   }
 
   metadata = {
-    serial-port-enable = 1
+    serial-port-enable = var.vm_web_serial_port_enable
     ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
   }
+}
 
+output "external_ip" {
+  value = yandex_compute_instance.platform.network_interface.0.nat_ip_address
 }
